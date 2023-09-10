5c45e72ae2c2c93c283d73dc64716a44f1845ed3
mohamedrashad102 <mmm682552@gmail.com>
2023-09-10 14:42:15 +0300

    first commit

---
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,53 @@
+import 'package:bookly_app/core/utils/assets.dart/my_assets.dart';
+import 'package:bookly_app/features/presentation/views/widgets/sliding_text.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _slidingAnimation =
+        Tween<Offset>(begin: const Offset(-2, 0), end: Offset.zero)
+            .animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          MyAssets.logo,
+          height: 50,
+          width: 100,
+        ),
+        const SizedBox(height: 10),
+        SlidingText(slidingAnimation: _slidingAnimation),
+      ],
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
